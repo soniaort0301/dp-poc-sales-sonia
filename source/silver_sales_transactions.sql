a63349d4049144686255e3873b52b63e39f9395c
sourtirane@meltone.com <sourtirane@meltone.com>
2026-09-15 12:08:24 +0000

    bronze + Silver

--- source/silver_sales_transactions.sql
+++ source/silver_sales_transactions.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE TABLE db_test_sonia.sch_silver.sales_transactions
 AS
 SELECT 
-  * EXCEPT (_ingestion_timestamp, _source_table, _layer),
+  *,
   current_timestamp() AS _ingestion_timestamp,
   'db_test_sonia.sch_bronze.sales_transactions' AS _source_table,
   'silver' AS _layer
