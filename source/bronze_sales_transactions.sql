dbb83ca67a4f5c8f45c84ba865d5904beeccf925
sourtirane@meltone.com <sourtirane@meltone.com>
2026-09-17 08:01:32 +0000

    Ajout batch_date + partitionnement + MERGE

--- source/bronze_sales_transactions.sql
+++ source/bronze_sales_transactions.sql
@@ -1,3 +1,4 @@
-CREATE OR REPLACE TABLE db_test_sonia.sch_bronze.sales_transactions
-AS
-SELECT * FROM samples.bakehouse.sales_transactions;
+INSERT INTO db_test_sonia.sch_bronze.sales_transactions
+REPLACE WHERE ingestion_date = :batch_date
+SELECT *, :batch_date AS ingestion_date
+FROM samples.bakehouse.sales_transactions;
--- source/silver_sales_transactions.sql
+++ source/silver_sales_transactions.sql
@@ -1,8 +1,34 @@
-CREATE OR REPLACE TABLE db_test_sonia.sch_silver.sales_transactions
-AS
-SELECT 
-  *,
-  current_timestamp() AS _ingestion_timestamp,
-  'db_test_sonia.sch_bronze.sales_transactions' AS _source_table,
-  'silver' AS _layer
-FROM db_test_sonia.sch_bronze.sales_transactions;
+MERGE INTO db_test_sonia.sch_silver.sales_transactions AS target
+USING (
+  SELECT 
+    *,
+    current_timestamp() AS _updated_at
+  FROM db_test_sonia.sch_bronze.sales_transactions
+  WHERE ingestion_date = :batch_date
+) AS source
+ON target.transactionID = source.transactionID
+
+WHEN MATCHED THEN 
+  UPDATE SET
+    target.customerID = source.customerID,
+    target.franchiseID = source.franchiseID,
+    target.dateTime = source.dateTime,
+    target.product = source.product,
+    target.quantity = source.quantity,
+    target.unitPrice = source.unitPrice,
+    target.totalPrice = source.totalPrice,
+    target.paymentMethod = source.paymentMethod,
+    target.ingestion_date = source.ingestion_date,
+    target._updated_at = source._updated_at
+
+WHEN NOT MATCHED THEN 
+  INSERT (
+    transactionID, customerID, franchiseID, dateTime, product, 
+    quantity, unitPrice, totalPrice, paymentMethod, ingestion_date,
+    _inserted_at, _updated_at
+  )
+  VALUES (
+    source.transactionID, source.customerID, source.franchiseID, source.dateTime, source.product,
+    source.quantity, source.unitPrice, source.totalPrice, source.paymentMethod, source.ingestion_date,
+    current_timestamp(), source._updated_at
+  );
