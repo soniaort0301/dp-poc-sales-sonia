d725fcb4a91fbc8bedcf1da2926902fbf44cece4
sourtirane@meltone.com <sourtirane@meltone.com>
2026-09-24 08:54:49 +0000

    Ajout ddl/create_tables.sql, MERGE ne update que si changement réel, partitionnement Bronze/Silver par date+heure

--- source/bronze_sales_transactions.sql
+++ source/bronze_sales_transactions.sql
@@ -1,4 +1,7 @@
 INSERT INTO db_test_sonia.sch_bronze.sales_transactions
-REPLACE WHERE ingestion_date = :batch_date
-SELECT *, :batch_date AS ingestion_date
+REPLACE WHERE ingestion_date = :batch_date AND ingestion_hour = :batch_hour
+SELECT 
+  *, 
+  :batch_date AS ingestion_date,
+  :batch_hour AS ingestion_hour
 FROM samples.bakehouse.sales_transactions;
--- source/silver_sales_transactions.sql
+++ source/silver_sales_transactions.sql
@@ -4,11 +4,20 @@ USING (
     *,
     current_timestamp() AS _updated_at
   FROM db_test_sonia.sch_bronze.sales_transactions
-  WHERE ingestion_date = :batch_date
+  WHERE ingestion_date = :batch_date AND ingestion_hour = :batch_hour
 ) AS source
 ON target.transactionID = source.transactionID
 
-WHEN MATCHED THEN 
+WHEN MATCHED AND (
+    target.customerID <> source.customerID OR
+    target.franchiseID <> source.franchiseID OR
+    target.dateTime <> source.dateTime OR
+    target.product <> source.product OR
+    target.quantity <> source.quantity OR
+    target.unitPrice <> source.unitPrice OR
+    target.totalPrice <> source.totalPrice OR
+    target.paymentMethod <> source.paymentMethod
+) THEN 
   UPDATE SET
     target.customerID = source.customerID,
     target.franchiseID = source.franchiseID,
@@ -19,16 +28,17 @@ WHEN MATCHED THEN
     target.totalPrice = source.totalPrice,
     target.paymentMethod = source.paymentMethod,
     target.ingestion_date = source.ingestion_date,
+    target.ingestion_hour = source.ingestion_hour,
     target._updated_at = source._updated_at
 
 WHEN NOT MATCHED THEN 
   INSERT (
     transactionID, customerID, franchiseID, dateTime, product, 
-    quantity, unitPrice, totalPrice, paymentMethod, ingestion_date,
+    quantity, unitPrice, totalPrice, paymentMethod, ingestion_date, ingestion_hour,
     _inserted_at, _updated_at
   )
   VALUES (
     source.transactionID, source.customerID, source.franchiseID, source.dateTime, source.product,
-    source.quantity, source.unitPrice, source.totalPrice, source.paymentMethod, source.ingestion_date,
+    source.quantity, source.unitPrice, source.totalPrice, source.paymentMethod, source.ingestion_date, source.ingestion_hour,
     current_timestamp(), source._updated_at
   );
